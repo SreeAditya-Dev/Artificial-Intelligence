e6af31c4867affcfce8d0fa5516e112758a98e98
Sree Aditya <165385350+SreeAditya-Dev@users.noreply.github.com>
2024-04-22 00:03:42 +0530

    Update train.py

--- Day_11/train.py
+++ Day_11/train.py
@@ -17,7 +17,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.models import model_from_json
 
-dataset = loadtxt('pima-indians-diabetes.csv', delimiter=',')
+dataset = loadtxt(r"C:\Users\Hp\OneDrive\Pictures\new technology\Artificial intelligence\Day_11\pima-indians-diabetes.csv", delimiter=',')
 x = dataset[:,0:8]
 y = dataset[:,8]
 print(x)
@@ -32,7 +32,7 @@ model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 #Model Training
-model.fit(x, y, epochs=40, batch_size=10)
+model.fit(x, y, epochs=50, batch_size=10)
 
 #Evaluation
 _, accuracy = model.evaluate(x, y)
@@ -42,5 +42,5 @@ print('Accuracy: %.2f' % (accuracy*100))
 model_json = model.to_json()
 with open("model.json", "w") as json_file:
     json_file.write(model_json)
-model.save_weights("model.h5")
+model.save_weights("model.weights.h5")
 print("Saved model to disk")
